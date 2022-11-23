734d1e83e3de5f65956b6b8d85b069c512238045
mustafa <hailiang.fang@maitang001.com>
2022-11-24 00:31:02 +0800

    test

--- src/LRUCacheManager.php
+++ src/LRUCacheManager.php
@@ -2,16 +2,18 @@
 
 namespace Mustafa\Lrucache;
 
+use Mustafa\Lrucache\Core\LRUCache;
+
 class LRUCacheManager
 {
     private static array $lruCacheList = [];
 
-    public static function register(string $entity, \Mustafa3264\Lrucache\Core\LRUCache $LRUCache): void
+    public static function register(string $entity, LRUCache $LRUCache): void
     {
         self::$lruCacheList[$entity] = $LRUCache;
     }
 
-    public static function instance(string $entity): \Mustafa3264\Lrucache\Core\LRUCache
+    public static function instance(string $entity): LRUCache
     {
         return self::$lruCacheList[$entity];
     }
