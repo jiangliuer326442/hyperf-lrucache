4fb066a65e6f2ff53f852f7ef236632dbb339f83
mustafa <hailiang.fang@maitang001.com>
2022-12-07 23:15:59 +0800

    test

--- src/Aspect/SwooleTableCacheAspect.php
+++ src/Aspect/SwooleTableCacheAspect.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mustafa\Lrucache\Aspect;
 
+use Hyperf\DbConnection\Collector\TableCollector;
 use Mustafa\Lrucache\Annotation\SwooleTable;
 use Mustafa\Lrucache\Annotation\SwooleTableCache;
 use Mustafa\Lrucache\LRUCacheManager;
@@ -11,25 +12,39 @@ use Hyperf\Di\Annotation\AnnotationCollector;
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
+use Psr\Container\ContainerInterface;
 
 #[Aspect(annotations: [SwooleTableCache::class])]
 class SwooleTableCacheAspect extends AbstractAspect
 {
+    protected TableCollector $collector;
+
+    public function __construct(\Hyperf\Contract\ContainerInterface $container){
+        $this->collector = $container->get(TableCollector::class);
+    }
+
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
         $annotation = AnnotationCollector::getClassMethodAnnotation($proceedingJoinPoint->className, $proceedingJoinPoint->methodName);
 
-        $table = make($annotation[SwooleTableCache::class]->entity)->getTable();
+        $model = make($annotation[SwooleTableCache::class]->entity);
+        $table = $model->getTable();
 
         $swooletableAnonotation = AnnotationCollector::getClassAnnotation($annotation[SwooleTableCache::class]->entity, SwooleTable::class);
 
         $cache = LRUCacheManager::instance($table);
 
         if ($annotation[SwooleTableCache::class]->action === 'get') {
-            $ret = $cache->get($table . ':', $proceedingJoinPoint->getArguments()[0]);
+            $data = $cache->get($table . ':', $proceedingJoinPoint->getArguments()[0]);
+            if ($data) {
+                $connection = $model->getConnectionName();
+                $defaultData = $this->collector->getDefaultValue(
+                    $connection,
+                    $table
+                );
+                $attributes = array_replace($defaultData, $data);
 
-            if ($ret) {
-                return $ret;
+                return $model->newFromBuilder($attributes);
             }
         }
 
