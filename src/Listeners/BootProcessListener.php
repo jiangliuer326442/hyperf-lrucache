d2ef0eba544c858673665cb19ca35bcfd569a569
mustafa <hailiang.fang@maitang001.com>
2022-12-07 21:09:32 +0800

    test

--- src/Listeners/BootProcessListener.php
+++ src/Listeners/BootProcessListener.php
@@ -31,7 +31,8 @@ class BootProcessListener implements \Hyperf\Event\Contract\ListenerInterface
     {
         $tables = $this->getAnnotationTables();
         foreach ($tables as $table_class => $swooletableobj) {
-            $table_name = $swooletableobj->table;
+            $instance = make($table_class);
+            $table_name = $instance->getTable();
             $swooleTableSize = $swooletableobj->swooleTableSize;
 
             $swooletable = new \Swoole\Table($swooleTableSize);
@@ -39,7 +40,7 @@ class BootProcessListener implements \Hyperf\Event\Contract\ListenerInterface
             $rows = $this->getAnnotationTableRowsByClass($table_class);
 
             foreach ($rows as $row) {
-                $swooletable->column($row['annotation']->name, $row['annotation']->type, $row['annotation']->length);
+                $swooletable->column($row[0], $row[1], $row[2]);
             }
             $swooletable->create();
             SwooleTableManage::register($table_name, $swooletable);
@@ -56,8 +57,29 @@ class BootProcessListener implements \Hyperf\Event\Contract\ListenerInterface
 
     private function getAnnotationTableRowsByClass(string $clazz): array
     {
-        return array_filter(AnnotationCollector::getPropertiesByAnnotation(SwooleTableItem::class), function ($item) use ($clazz) {
-            return $item['class'] === $clazz;
-        });
+        $reflection = new \ReflectionClass($clazz);
+        $doc = $reflection->getDocComment();
+        $lines = explode("\n", $doc);
+        $list = [];
+        foreach ($lines as $line){
+            if (strstr($line, '@property')){
+                $cells = explode(" ", $line);
+                if ($cells[3] == 'int'){
+                    $type = \Swoole\Table::TYPE_INT;
+                    $length = 11;
+                }else{
+                    $type = \Swoole\Table::TYPE_STRING;
+                    $length = 255;
+                }
+                $name = substr($cells[4], 1);
+                $list[] = [$name, $type, $length];
+            }
+        }
+        $delete_at = defined('static::DELETED_AT') ? static::DELETED_AT : 'deleted_at';
+        $list[] = [$delete_at, \Swoole\Table::TYPE_STRING, 64];
+        $list[] = ["_pre", \Swoole\Table::TYPE_STRING, 64];
+        $list[] = ["_next", \Swoole\Table::TYPE_STRING, 64];
+        $list[] = ["_expire", \Swoole\Table::TYPE_INT, 64];
+        return $list;
     }
 }
